b770e5c21dfb2ef5ea41f12a59ba9fda1728b923
Quazi Heider <qheider@gmail.com>
2016-09-18 15:39:44 -0400

    REST commit
    hgfhg

---
+++ RestClient/src/RestClientMain.java
@@ -0,0 +1,71 @@
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import org.apache.http.client.ClientProtocolException;
+import org.jboss.resteasy.client.ClientRequest;
+import org.jboss.resteasy.client.ClientResponse;
+
+public class RestClientMain {
+
+	
+		public static void main(String[] args) {
+			  try {
+
+				ClientRequest request = new ClientRequest(
+						"http://localhost:9090/javaMonkey/rest/xml/user/get");
+				request.accept("application/xml");
+				
+		
+				
+				
+				ClientResponse<User> response = request.get(User.class);
+
+				if (response.getStatus() != 200) {
+					throw new RuntimeException("Failed : HTTP error code : "
+						+ response.getStatus());
+				}
+
+//				BufferedReader br = new BufferedReader(new InputStreamReader(
+//					new ByteArrayInputStream(response.getEntity().getBytes())));
+//
+//				String output;
+//				System.out.println("Output from Server .... \n");
+//				while ((output = br.readLine()) != null) {
+//					System.out.println(output);
+//				}
+
+				
+				  User user = response.getEntity();
+//				  String userName=response.getEntity().getUsername();
+//				  String passWord= response.getEntity().getPassword();
+//				  String pin =response.getEntity().getPin();
+//				  
+//				     User user=new User();
+//				     user.setUsername(userName);
+//				     user.setPassword(passWord);
+//				     user.setPin(pin);
+				    //verify the user object
+				    System.out.println(user.getUsername());
+				    System.out.println(user.getPassword());
+				    System.out.println(user.getPin());
+				
+				
+			  } catch (ClientProtocolException e) {
+
+				e.printStackTrace();
+
+			  } catch (IOException e) {
+
+				e.printStackTrace();
+
+			  } catch (Exception e) {
+
+				e.printStackTrace();
+
+			  }
+
+			}
+
+
+}
---
+++ javaMonkey/src/com/mkyong/rest/XMLService.java
@@ -0,0 +1,45 @@
+package com.mkyong.rest;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+@Path("/xml/user")
+public class XMLService {
+
+	@GET
+	@Path("/get")
+	@Produces("application/xml")
+	public User getUserInXML() {
+
+		User user = new User();
+		user.setUsername("mkyong");
+		user.setPassword("password");
+		user.setPin(generatePin());
+		
+		return user; 
+
+	}
+
+	private String generatePin() {
+		String pin =    getRandomTriple() + "-" +
+                getRandomTriple() + "-" +
+                getRandomTriple() + "-" +
+                getRandomTriple();
+System.out.println("Generated " + pin);
+return pin;
+} 
+public static int getRandomDigit() {
+return (int)(10.0 * Math.random());
+} 
+public static char getRandomChar() {
+return (char)('A' + (26.0 * Math.random()) );
+}
+
+public static String getRandomTriple() {
+return "" + getRandomDigit() + getRandomDigit() + getRandomChar();
+} 
+
+	
+
+}
